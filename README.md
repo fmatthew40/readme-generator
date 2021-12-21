b795d360f57ec0755af04b6ab33ca1475168ab07
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2021-12-21 14:19:13 -0700

    writefile function added

---
+++ README.md
@@ -0,0 +1,7 @@
+
+  
+  # tit;
+
+ #Description: describ;
+
+
--- index.js
+++ index.js
@@ -1,17 +1,79 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+var inquirer = require('inquirer');
+
+const generatePage = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: 'input',
+    message: 'Please provide an email for users to contact you.',
+    name: 'email'
+},  
+{
+    type: 'input',
+    message: 'What is your github username?',
+    name: 'github'
+}, 
+{
+    type: 'input',
+    message:  'What is the title of your project?',
+    name: 'title'
+},
+{
+    type: 'input',
+    message:  'Please describe your project in detail.',
+    name: 'description'
+},
+{
+    type: 'input',
+    message:  'Please provide your installation instructions.',
+    name: 'installation'
+},    
+{
+    type: 'input',
+    message:  'What is the projects intended usage?',
+    name: 'usage'
+},    
+{
+    type: 'input',
+    message:  'License?',
+    name: 'license'
+},  
+{
+    type: 'input',
+    message: 'How can a user contribute to the project or repository?',
+    name: 'contributing'
+},  
+{
+    type: 'input',
+    message: 'What tests have been performed on the project and how can a user perform the tests?',
+    name: 'tests'
+},  
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const newfile = (fileName, answers)  =>{
+    let content = generatePage(answers);
+    fs.writeFile(fileName, content, function (error) {
+        if (error) {
+            return console.log(error)
+        }
+        console.log('Your readme was created successfully!')
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+const init =() => {
+    inquirer.prompt(questions).then(function (answers) {
+        const fileName = 'README.md';
+        newfile(fileName, answers)
+    });
+}
 
 // Function call to initialize app
 init();
 
-
-var message ='Hello There';
-console.log(message);
